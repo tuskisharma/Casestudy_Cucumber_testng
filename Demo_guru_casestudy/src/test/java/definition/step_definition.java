124e1a6dfe5e008e86d9583e039eb7cf620f2af3
tuskisharma <tushar235553@gmail.com>
2022-11-07 20:21:45 +0530

    Demo_guru_casestudy_selenium

---
+++ Demo_guru_casestudy/src/test/java/definition/step_definition.java
@@ -0,0 +1,47 @@
+package definition;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class step_definition {
+	public static String url="https://demo.guru99.com/test/newtours/";
+	WebDriver driver;
+	@When("Enter the username {string}")
+	public void enter_the_username(String string) {
+		WebDriverManager.chromedriver().setup();
+	    driver = new ChromeDriver();
+	    driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		driver.get(url);
+		driver.manage().window().maximize();
+		driver.findElement(By.xpath("//*[@name='userName']")).sendKeys(string);
+	}
+	@When("Enter the password {string}")
+	public void enter_the_password(String string) {
+		
+		driver.findElement(By.xpath("//*[@name='password']")).sendKeys(string);
+		
+	}
+	@When("Click on the login button")
+	public void click_on_the_login_button() {
+		driver.findElement(By.xpath("//*[@name='submit']")).click();
+	}
+	@Then("I should see title as {string}")
+	public void i_should_see_title_as(String string)throws InterruptedException {
+	  Assert.assertEquals(string, driver.getTitle());
+	  Thread.sleep(3000);
+	  driver.close();
+	}
+	
+}
+
+
